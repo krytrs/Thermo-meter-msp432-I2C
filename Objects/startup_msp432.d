ffa7c462af0eebeac036b36b994f750f0b3cea4f
krytrs <krytrs@gmail.com>
2016-04-01 16:52:00 +0200

    Revert "Delete all files and start from new"
    This reverts commit 65f562c7866e92ad292fc0707c3ab301223a2820.

---
+++ Objects/startup_msp432.d
@@ -0,0 +1,1 @@
+.\objects\startup_msp432.o: RTE\Device\MSP432P401R\startup_msp432.s
---
+++ Objects/system_msp432.d
@@ -0,0 +1,11 @@
+.\objects\system_msp432.o: RTE\Device\MSP432P401R\system_msp432.c
+.\objects\system_msp432.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\system_msp432.o: C:\Keil_v5\ARM\PACK\TexasInstruments\MSP432\1.0.3\Device\Include\msp.h
+.\objects\system_msp432.o: C:\Keil_v5\ARM\PACK\TexasInstruments\MSP432\1.0.3\Device\Include\msp432p401r.h
+.\objects\system_msp432.o: C:\Keil_v5\ARM\PACK\TexasInstruments\MSP432\1.0.3\Device\Include\msp_compatibility.h
+.\objects\system_msp432.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.5.0\CMSIS\Include\core_cm4.h
+.\objects\system_msp432.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.5.0\CMSIS\Include\core_cmInstr.h
+.\objects\system_msp432.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.5.0\CMSIS\Include\cmsis_armcc.h
+.\objects\system_msp432.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.5.0\CMSIS\Include\core_cmFunc.h
+.\objects\system_msp432.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.5.0\CMSIS\Include\core_cmSimd.h
+.\objects\system_msp432.o: C:\Keil_v5\ARM\PACK\TexasInstruments\MSP432\1.0.3\Device\Include\system_msp432.h
