861ea490fab29126cc1238269e4d0eb4d8937ebe
krytrs <krytrs@gmail.com>
2016-04-02 13:29:45 +0200

    Rozchozeny tak nejak displej

--- main.c
+++ main.c
@@ -9,12 +9,14 @@
 
 void main(void)
 {
+	uint16_t i;
 	
     WDTCTL = WDTPW | WDTHOLD;           // Stop watchdog timer
 	
     LCD_init();
     LCD_clearScreen();
     LCD_setCursorPosition(0, 0);
+    for (i = 0; i < 60000; i++);
     LCD_printStr("Petr je pasak");
 
 
