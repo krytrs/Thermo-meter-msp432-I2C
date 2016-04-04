75ed971f9bd9fcd71054758b14223abb0ed79184
krytrs <krytrs@gmail.com>
2016-04-04 13:56:43 +0200

    Rozepsani i2c
    TODO

---
+++ I2C.h
@@ -0,0 +1,16 @@
+/*
+ * I2C.h
+ *
+ *  Created on: 4. 4. 2016
+ *      Author: Krytrs
+ */
+
+#ifndef I2C_H_
+#define I2C_H_
+
+#define BK_I2C_TRANSMIT    ((uint16_t)0x0010)
+#define BK_I2C_TRANSMIT    ((uint16_t)0x0000)
+
+
+
+#endif /* I2C_H_ */
